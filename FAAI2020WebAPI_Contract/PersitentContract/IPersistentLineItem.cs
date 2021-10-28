b8a8d81b430edf88b535a74f22c2f2551373c72e
Markus Steinbacher <Markus Steinbacher@DESKTOP-H11HN79>
2021-10-28 18:59:54 +0100

    LineItem interface hinzugefügt

---
+++ FAAI2020WebAPI_Contract/PersitentContract/IPersistentLineItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAAI2020WebAPI_Contract.PersitentContract
+{
+    public interface IPersistentLineItem
+    {
+        public string ArticleId { get; set; }
+        public double Quantity { get; set; }
+
+        public string Text { get; set; }
+    }
+}
